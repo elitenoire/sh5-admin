38d441c38252d1930ad2ba178ada720019ad6586
Eva Raymond <evathecoderr@outlook.com>
2022-05-21 09:37:49 +0100

    style change

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,11 +1,11 @@
 function ProductCard({ product }) {
 	return (
-		<div className="ds-card w-full max-w-xs mx-auto bg-base-100 dark:bg-neutral/50">
+		<div className="ds-card w-full max-w-xs mx-auto bg-base-100 dark:bg-neutral/50 hover:shadow-lg hover:shadow-primary/10  dark:shadow-neutral/90 transition-[transform,box-shadow] duration-300 hover:-translate-y-1">
 			<figure className="px-2 pt-2">
 				<img src={product.image} alt={product.name} className="rounded-xl" />
 			</figure>
-			<div className="ds-card-body">
-				<h2 className="ds-card-title">{product.name}</h2>
+			<div className="ds-card-body gap-1 p-6">
+				<h2 className="ds-card-title mb-2 leading-tight">{product.name}</h2>
 				<p className="flex justify-between items-center">
 					<span className="">Net Price:</span>
 					<span className="">{`$${product.net_price}`}</span>
